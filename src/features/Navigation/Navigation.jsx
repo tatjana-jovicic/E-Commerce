4383e394e650527312a6a98ccd217803ef0ab61a
unknown <tatjanajovicic19@gmail.com>
2024-05-21 13:25:51 +0200

    commit from feat-Navigation

--- src/features/Navigation/Navigation.jsx
+++ src/features/Navigation/Navigation.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
 import "./Navigation.css";
+import PageLogo from "../../components/PageLogo/PageLogo";
+import Cart from "../../components/Cart/Cart";
 
 const Navigation = () => {
   return (
     <nav className="nav">
-      <Link to={{ pathname: "/" }}>Home</Link>
-      <Link to={{ pathname: "/offer" }}>Offer</Link>
-      <Link to={{ pathname: "/how_order" }}>How to order?</Link>
-      <Link to={{ pathname: "/about_us" }}>About Us</Link>
-      <Link to={{ pathname: "/contact" }}>Contact</Link>
+      <PageLogo />
+      <div className="nav_links">
+        <Link to={{ pathname: "/" }}>Home</Link>
+        <Link to={{ pathname: "/offer" }}>Offer</Link>
+        <Link to={{ pathname: "/how_order" }}>How to order?</Link>
+        <Link to={{ pathname: "/about_us" }}>About Us</Link>
+        <Link to={{ pathname: "/contact" }}>Contact</Link>
+      </div>
+      <Cart />
     </nav>
   );
 };
