8a8c6c6bac2665ac93c32d24b94c0f2d5ae34650
unknown <tatjanajovicic19@gmail.com>
2024-05-21 18:17:49 +0200

    first commit from feat-Home

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,6 +1,33 @@
 import "./Home.css";
+import Button from "../../components/Button/Button";
+import HeadphonesBlack from "../../assets/headphones-black.png";
+import HomeFooter from "./components/HomeFooter";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="nesto">
+        <div className="home_div">
+          <div className="home_div_left">
+            <div className="home_title">
+              <h1>Fresh Beautiful.</h1>
+              <h1>Innovative.</h1>
+            </div>
+            <hr />
+            <p>
+              There are many variations of passangers of Lorem Ipsum available,
+              but the majority have suffered alteration in some from, by
+              injected humor
+            </p>
+            <Button buttonText="ORDER NOW" />
+          </div>
+          <div className="home_div_right">
+            <img src={HeadphonesBlack} alt="headphones black" />
+          </div>
+        </div>
+        <HomeFooter />
+      </div>
+    </>
+  );
 };
 export default Home;
