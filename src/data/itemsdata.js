aee7a5a3925829f3f249690650e0eb95ebf953dd
unknown <tatjanajovicic19@gmail.com>
2024-06-07 15:06:21 +0200

    add components for Offer from feat-Offer

---
+++ src/data/itemsdata.js
@@ -0,0 +1,167 @@
+import headphones1 from "../assets/items/headphones1.webp";
+import headphones2 from "../assets/items/headphones2.webp";
+import headphones3 from "../assets/items/headphones3.webp";
+import headphones4 from "../assets/items/headphones4.webp";
+import headphones5 from "../assets/items/headphones5.webp";
+import headphones6 from "../assets/items/headphones6.webp";
+import headphones7 from "../assets/items/headphones7.webp";
+import headphones8 from "../assets/items/headphones8.webp";
+import headphones9 from "../assets/items/headphones9.webp";
+import headphones10 from "../assets/items/headphones10.webp";
+import headphones11 from "../assets/items/headphones11.webp";
+import headphones12 from "../assets/items/headphones12.webp";
+import headphones13 from "../assets/items/headphones13.webp";
+import headphones14 from "../assets/items/headphones14.webp";
+import headphones15 from "../assets/items/headphones15.webp";
+import headphones16 from "../assets/items/headphones16.webp";
+import headphones17 from "../assets/items/headphones17.webp";
+import headphones18 from "../assets/items/headphones18.webp";
+import headphones19 from "../assets/items/headphones19.webp";
+import headphones20 from "../assets/items/headphones20.webp";
+
+export const items = [
+  {
+    id: 1,
+    name: "Item 1",
+    price: 122,
+    image: headphones1,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 2,
+    name: "Item 2",
+    price: 432,
+    image: headphones2,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 3,
+    name: "Item 3",
+    price: 544,
+    image: headphones3,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 4,
+    name: "Item 4",
+    price: 432,
+    image: headphones4,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 5,
+    name: "Item 5",
+    price: 432,
+    image: headphones5,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 6,
+    name: "Item 6",
+    price: 432,
+    image: headphones6,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 7,
+    name: "Item 7",
+    price: 432,
+    image: headphones7,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 8,
+    name: "Item 8",
+    price: 432,
+    image: headphones8,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 9,
+    name: "Item 9",
+    price: 432,
+    image: headphones9,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 10,
+    name: "Item 10",
+    price: 432,
+    image: headphones10,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 11,
+    name: "Item 11",
+    price: 432,
+    image: headphones11,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 12,
+    name: "Item 12",
+    price: 432,
+    image: headphones12,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 13,
+    name: "Item 13",
+    price: 432,
+    image: headphones13,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 14,
+    name: "Item 14",
+    price: 432,
+    image: headphones14,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 15,
+    name: "Item 15",
+    price: 432,
+    image: headphones15,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 16,
+    name: "Item 16",
+    price: 432,
+    image: headphones16,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 17,
+    name: "Item 17",
+    price: 432,
+    image: headphones17,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 18,
+    name: "Item 18",
+    price: 432,
+    image: headphones18,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 19,
+    name: "Item 19",
+    price: 432,
+    image: headphones19,
+    description: "Simple design, high quality sound.",
+  },
+  {
+    id: 20,
+    name: "Item 20",
+    price: 432,
+    image: headphones20,
+    description: "Simple design, high quality sound.",
+  },
+];
+
+export const getAllItems = () => {
+  return items;
+};
--- src/features/Offer/Offer.jsx
+++ src/features/Offer/Offer.jsx
@@ -1,6 +1,16 @@
 import "./Offer.css";
+import Item from "./components/Item";
+import { items } from "../../data/itemsdata";
 
 const Offer = () => {
-  return <></>;
+  return (
+    <>
+      <div className="items">
+        {items.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </div>
+    </>
+  );
 };
 export default Offer;
