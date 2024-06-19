a38894976a388f1915cec9ad3c6d235b7190be68
unknown <tatjanajovicic19@gmail.com>
2024-06-19 12:57:11 +0200

    update star rating from feat-Offer

--- src/features/Offer/components/TextRating.jsx
+++ src/features/Offer/components/TextRating.jsx
@@ -3,19 +3,6 @@ import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import StarIcon from "@mui/icons-material/Star";
 
-const labels = {
-  0.5: "Useless",
-  1: "Useless+",
-  1.5: "Poor",
-  2: "Poor+",
-  2.5: "Ok",
-  3: "Ok+",
-  3.5: "Good",
-  4: "Good+",
-  4.5: "Excellent",
-  5: "Excellent+",
-};
-
 const TextRating = ({ rating }) => {
   return (
     <Box
@@ -30,9 +17,13 @@ const TextRating = ({ rating }) => {
         value={rating}
         readOnly
         precision={0.5}
-        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+        emptyIcon={<StarIcon style={{ opacity: 0.55, fontSize: "17px" }} />}
+        icon={
+          <StarIcon
+            style={{ fontSize: "17px", backgroundColor: "lightgray" }}
+          />
+        }
       />
-      <Box sx={{ ml: 2 }}></Box>
     </Box>
   );
 };
